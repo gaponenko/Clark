155592dd57e66287730dea3d41fe2134df1a43a1
Andrei Gaponenko <gandr@fnal.gov>
2013-02-20 23:56:05 -0600

    Fix a bug in cluster TDC width computation (affected the PACT cut).

--- WireCluster.cpp
+++ WireCluster.cpp
@@ -33,7 +33,7 @@ WireCluster::WireCluster(const TDCHitWPPtrCollection& in)
 //================================================================
 double WireCluster::totalTDCWidth() const {
   double res(0);
-  for(TDCHitWPPtrCollection::const_iterator i= ++hits_.begin(); i!=hits_.end(); ++i) {
+  for(TDCHitWPPtrCollection::const_iterator i= hits_.begin(); i!=hits_.end(); ++i) {
     res += (*i)->width;
   }
   return res;
