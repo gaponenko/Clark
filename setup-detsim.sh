12b3e8c6e3522de4741dfb4ef2ce8e9fe6ccc4dd
Andrei Gaponenko <gandr@fnal.gov>
2013-05-03 11:08:23 -0500

    Add a detsim-specific environment setup file.

---
+++ setup-detsim.sh
@@ -0,0 +1,7 @@
+# setup for detsim
+export CAL_DB=/mu2e/data/users/gandr/twist/caldb_ascii
+setup mu2e
+setup gsl v1_15a
+setup log4cpp v1_0
+setup boost v1_53_0 -q e2:prof
+setup root v5_34_05 -q e2:prof
