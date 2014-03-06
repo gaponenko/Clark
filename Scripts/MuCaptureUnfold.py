adf8b478b1fce85ddcb5a6dcfa49a15066f8ac36
Ant OnPositron <ant@uvic.ca>
2014-03-05 17:01:54 -0800

    Add logy option.Calculate & draw chi2 calc by hand

--- Scripts/MuCaptureUnfold.py
+++ Scripts/MuCaptureUnfold.py
@@ -56,6 +56,11 @@ class Unfolder:
 		else:
 			self.output = options.output
 
+		if options.logy:
+		    self.logy = True
+		else:
+		    self.logy = False
+
 		self.Canv = TCanvas()
 		self.Canv.Print(self.output+'[')
 
@@ -73,21 +78,44 @@ class Unfolder:
 	
 	def PrintMeasured(self):
 
-		def DrawMeasuredFromResponse(thefile, thetitle):
+		if self.logy:
+			self.Canv.SetLogy()
+		def DrawMeasuredFromResponse(thefile, thetitle, legend):
 			response = thefile.Get("MuCapture/anDnLateResponse")
-			hMeas = response.Hmeasured()
+			recomeas = response.Hmeasured()
 			if not thetitle == None:
-				hMeas.SetTitle(thetitle+" measured spectrum")
-			hMeas.Draw()
+				recomeas.SetTitle(thetitle+" measured spectrum")
+			recomeas.Draw()
+			Maxreco = recomeas.GetMaximum()
+
+			truemeas = thefile.Get("MuCapture/LateResponse/MCTruthMomentumReco")
+			truemeas.SetLineColor(2)
+			Maxtrue = truemeas.GetMaximum()
+
+			MaxBin = max(Maxreco, Maxtrue)
+			# recomeas.SetAxisRange(-(0.1*MaxBin),(1.1*MaxBin),'Y')
+			recomeas.SetAxisRange(1e-6,(1.1*MaxBin),'Y')
+			
+			legend.AddEntry(recomeas, 'Reconstructed','L')
+			legend.AddEntry(truemeas, 'Truth for fitted trks', 'L')
+			truemeas.Draw("same")
 		
-		DrawMeasuredFromResponse(self.training, self.trainingTitle)
+
+		leg = TLegend(0.65,0.7,0.98,0.9)
+		DrawMeasuredFromResponse(self.training, self.trainingTitle, leg)
+		leg.Draw()
 		self.Canv.Print(self.output)
 		self.Canv.Clear()
 
 		if self.unfoldIsMC:
-			DrawMeasuredFromResponse(self.unfold, self.unfoldTitle)
+			leg.Clear()
+			DrawMeasuredFromResponse(self.unfold, self.unfoldTitle, leg)
+			leg.Draw()
 			self.Canv.Print(self.output)
 			self.Canv.Clear()
+
+		if self.logy:
+			self.Canv.SetLogy(0)
 
 
 	def PrintResponseMatrix(self):
@@ -120,20 +148,28 @@ class Unfolder:
 			print "ERROR: No unfolding performed yet !!!"
 			sys.exit(1)
 
+		if self.logy:
+			self.Canv.SetLogy()
 		hReco= self.unfoldresult.Hreco()
 
 		if self.unfoldIsMC:
 			unfoldTruth = self.unfold.Get("MuCapture/LateResponse/MCTruthMomentum")
 			unfoldTruth.SetLineColor(2)
-			leg = TLegend(0.65,0.5,0.98,0.7)
+			leg = TLegend(0.65,0.7,0.98,0.9)
 
 			MaxhReco = hReco.GetMaximum()
+			MinhReco = 99999.
+			for b in range(1,hReco.GetNbinsX()+1):
+				Min = hReco.GetBinContent(b)
+				if Min > 0.0:
+					MinhReco = min(Min,MinhReco)
 			unfoldTruth.Draw()
 			hReco.Draw("same")
 			leg.AddEntry(unfoldTruth, 'True input spectrum','L')
 			leg.AddEntry(hReco, 'Unfolded spectrum', 'L')
 			unfoldTruth.SetTitle("%s spectrum unfolded with %s spectrum using RooUnfold%s" % (self.unfoldTitle, self.trainingTitle, self.unfoldname))
-			unfoldTruth.SetAxisRange(0.0,(1.1*MaxhReco),'Y')
+			unfoldTruth.SetAxisRange((0.1*MinhReco),(1.1*MaxhReco),'Y')
+			print " =====> ",MinhReco,(0.1*MinhReco)
 			leg.Draw()
 		else:
 			hReco.Draw()
@@ -141,9 +177,12 @@ class Unfolder:
 		self.Canv.Update()
 		self.Canv.Print(self.output)
 		self.Canv.Clear()
+		if self.logy:
+			self.Canv.SetLogy(0)
 
 	def MakeChi2VsIterationPlot(self):
-		self.Chi2VsIter = TGraph()
+		self.RUChi2VsIter = TGraph()
+		self.MyChi2VsIter = TGraph()
 
 	def AddChi2VsIterationPoint(self):
 		if self.unfoldresult == None:
@@ -156,17 +195,38 @@ class Unfolder:
 
 		unfoldTruth = self.unfold.Get("MuCapture/LateResponse/MCTruthMomentum")
 		chi2 = self.unfoldresult.Chi2(unfoldTruth)
-		nbPt = self.Chi2VsIter.GetN()
-		self.Chi2VsIter.SetPoint(nbPt, self.nbiterations, chi2)
+		nbPt = self.RUChi2VsIter.GetN()
+		self.RUChi2VsIter.SetPoint(nbPt, self.nbiterations, chi2)
+
+		chi2 = 0
+		hReco= self.unfoldresult.Hreco()
+		for b in range(9,17):
+			yreco = hReco.GetBinContent(b)
+			eyreco = hReco.GetBinError(b)
+			ytrue = unfoldTruth.GetBinContent(b)
+			chi2 = chi2 + ( (yreco-ytrue) * (yreco-ytrue) / (eyreco*eyreco))
+		nbPt = self.MyChi2VsIter.GetN()
+		self.MyChi2VsIter.SetPoint(nbPt, self.nbiterations, chi2)
+
 
 
 	def DrawChi2VsIterationPlot(self):
 		self.Canv.Clear()
-		self.Chi2VsIter.SetTitle("chi^{2} between unfolded and true %s spectra, using %s spectrum;Number of iterations;chi^{2}" % (self.unfoldTitle,self.trainingTitle))
-		self.Chi2VsIter.SetMarkerStyle(5)
-		self.Chi2VsIter.SetMarkerColor(4)
-		self.Chi2VsIter.SetLineColor(4)
-		self.Chi2VsIter.Draw("APL")
+		self.RUChi2VsIter.SetTitle("RooUnfold chi^{2} between unfolded and true %s spectra, using %s spectrum;Number of iterations;chi^{2}" % (self.unfoldTitle,self.trainingTitle))
+		self.RUChi2VsIter.SetMarkerStyle(5)
+		self.RUChi2VsIter.SetMarkerColor(4)
+		self.RUChi2VsIter.SetLineColor(4)
+		self.RUChi2VsIter.Draw("APL")
+		self.Canv.Update()
+		self.Canv.Print(self.output)
+		self.Canv.Clear()
+
+		self.Canv.Clear()
+		self.MyChi2VsIter.SetTitle("Manual chi^{2} between unfolded and true %s spectra, using %s spectrum;Number of iterations;chi^{2}" % (self.unfoldTitle,self.trainingTitle))
+		self.MyChi2VsIter.SetMarkerStyle(5)
+		self.MyChi2VsIter.SetMarkerColor(4)
+		self.MyChi2VsIter.SetLineColor(4)
+		self.MyChi2VsIter.Draw("APL")
 		self.Canv.Update()
 		self.Canv.Print(self.output)
 		self.Canv.Clear()
@@ -183,8 +243,9 @@ class Unfolder:
 # parser = OptionParser(description=description)
 parser = OptionParser()
 parser.add_option("-t", "--training", dest="training", help="Root file containing the rooUnfold response.")
-parser.add_option("-u", "--unfold", dest="unfold", help="Root file containing the measured spectrum you want to unfold")
-parser.add_option("-o", "--output", dest="output", help="Output file name for the pdf file. Don't put the suffix '.pdf'", default="output")
+parser.add_option("-u", "--unfold", dest="unfold", help="Root file containing the measured spectrum you want to unfold.")
+parser.add_option("-o", "--output", dest="output", help="Output file name for the pdf file. Don't put the suffix '.pdf'.", default="output")
+parser.add_option("", "--logy", dest="logy", help="Use SetLogY for the 1D plots.", action="store_true")
 parser.add_option("-i", "--iterationcheck", dest="iterationcheck", help="Plot the unfolding results versus the number of iterations.", action="store_true")
 (options, args) = parser.parse_args()
 
