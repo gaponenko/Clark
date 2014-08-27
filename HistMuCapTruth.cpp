9cefb2e64bd40b818e3eeacf50d9f3ed0fd5a77e
Andrei Gaponenko <gandr@fnal.gov>
2014-08-27 10:35:14 -0500

    Use a broader momentum range for some MC truth histograms.

--- HistMuCapTruth.cpp
+++ HistMuCapTruth.cpp
@@ -21,10 +21,10 @@ void HistMuCapTruth::init(HistogramFactory &hf,
 
   hNumMCCaptureTracks_ = hf.DefineTH1D(hdir, "numCaptureMcTrkCandidates", "numCaptureMcTrkCandidates", 10, -0.5, 9.5);
   hCaptureTime_ = hf.DefineTH1D(hdir, "time", "MC time", 640, -100., 1500.);
-  hptot_ = hf.DefineTH1D(hdir, "ptot", "MC ptot", 200, 0., 200.);
+  hptot_ = hf.DefineTH1D(hdir, "ptot", "MC ptot", 300, 0., 300.);
   hek_ = hf.DefineTH1D(hdir, "ek", "MC Ek", 200, 0., 50.);
   hphi_ = hf.DefineTH1D(hdir, "phi", "MC momentum phi", 100, -PI, +PI);
-  hpcos_ = hf.DefineTH2D(hdir, "cosVsPtos", "MC cosTheta vs ptot", 200, 0., 200., 100, -1., 1.);
+  hpcos_ = hf.DefineTH2D(hdir, "cosVsPtos", "MC cosTheta vs ptot", 300, 0., 300., 100, -1., 1.);
   hpcos_->SetOption("colz");
 
   hVUend_ = hf.DefineTH2D(hdir, "uvend", "V vs U primary end vtx for |Zend|<60 cm", 600, -30., 30., 600, -30., 30.);
@@ -33,10 +33,10 @@ void HistMuCapTruth::init(HistogramFactory &hf,
   hRZend_ = hf.DefineTH2D(hdir, "rzend", "Primary end vtx R vs Z", 1200, -60., 60., 300, 0., 30.);
   hRZend_->SetOption("colz");
 
-  hRendVsPstart_ = hf.DefineTH2D(hdir, "rpstart", "End R vs start ptot for |Zend|<60 cm", 200, 0., 200., 300, 0., 30.);
+  hRendVsPstart_ = hf.DefineTH2D(hdir, "rpstart", "End R vs start ptot for |Zend|<60 cm", 300, 0., 300., 300, 0., 30.);
   hRendVsPstart_->SetOption("colz");
 
-  hZendVsPstart_ = hf.DefineTH2D(hdir, "zpstart", "End Z vs start ptot", 200, 0., 200., 1200, -60., 60.);
+  hZendVsPstart_ = hf.DefineTH2D(hdir, "zpstart", "End Z vs start ptot", 300, 0., 300., 1200, -60., 60.);
   hZendVsPstart_->SetOption("colz");
 
   // Same binning as G3 H4
