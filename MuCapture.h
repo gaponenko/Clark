2e7c7c041ef2eff7c317e522af28ae02690b2c3b
Andrei Gaponenko <gandr@fnal.gov>
2014-10-11 09:15:23 -0500

    Make the code usable on machines where RooUnfold is not available.

--- MuCapture.h
+++ MuCapture.h
@@ -41,6 +41,10 @@
 #include "HistTrkClass.h"
 #include "HistMuCapMCTgtStops.h"
 
+//#include "RooUnfold/RooUnfoldResponse.h"
+#include "RooUnfoldDummy.h"
+typedef RooUnfoldDummy RooUnfoldResponse;
+
 #include "TAxis.h"
 class TH1;
 class TH2;
