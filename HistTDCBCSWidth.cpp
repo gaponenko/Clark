2095fbae48f39722cbf3141d1685782ba2c4cc47
Andrei Gaponenko <gandr@fnal.gov>
2014-12-12 17:16:33 -0600

    New histogramming class to look at TDC hits in more detail.

---
+++ HistTDCBCSWidth.cpp
@@ -0,0 +1,88 @@
+// Andrei Gaponenko, 2014
+
+#include "HistTDCBCSWidth.h"
+#include <limits>
+#include <cmath>
+#include <algorithm>
+
+#include "HitBasedObservables.h"
+#include "EventClass.h"
+
+#include "TH1.h"
+#include "TH2.h"
+
+#include "HistogramFactory.h"
+#include "ConfigFile.h"
+#include "MuCapUtilities.h"
+
+#include "TimeWindow.h"
+
+extern TimeWindowingResults *gwres;
+
+//#define AGDEBUG(stuff) do { std::cerr<<"AG: "<<__FILE__<<", line "<<__LINE__<<", func "<<__func__<<": "<<stuff<<std::endl; } while(0)
+#define AGDEBUG(stuff)
+
+//================================================================
+void HistTDCBCSWidth::init(HistogramFactory& hf,
+                           const std::string& hdir,
+                           const DetectorGeo& geom,
+                           const ConfigFile& conf)
+{
+  geom_ = &geom;
+
+  clusterSizePC_ = hf.DefineTH2D(hdir, "clusterSizePC", "PC plane vs cluster size", 10, -0.5, 9.5, 12, 0.5, 12.5);
+  clusterSizePC_->SetOption("colz");
+  clusterSizePC_->GetXaxis()->SetTitle("PC cluster size");
+  clusterSizePC_->GetYaxis()->SetTitle("PC plane number");
+
+  clusterSizeDC_ = hf.DefineTH2D(hdir, "clusterSizeDC", "DC plane vs cluster size", 10, -0.5, 9.5, 44, 0.5, 44.5);
+  clusterSizeDC_->SetOption("colz");
+  clusterSizeDC_->GetXaxis()->SetTitle("DC cluster size");
+  clusterSizeDC_->GetYaxis()->SetTitle("DC plane number");
+
+  clusterSizeBinsPC_.resize(maxClusterSize);
+  for(int i=1; i<=maxClusterSize; ++i) {
+    std::ostringstream os;
+    os<<hdir<<"/pcClusterSize"<<i;
+    clusterSizeBinsPC_[i-1].init(os.str(), "pcwidth", 12, hf, conf);
+  }
+
+  clusterSizeBinsDC_.resize(maxClusterSize);
+  for(int i=1; i<=maxClusterSize; ++i) {
+    std::ostringstream os;
+    os<<hdir<<"/dcClusterSize"<<i;
+    clusterSizeBinsDC_[i-1].init(os.str(), "dcwidth", 44, hf, conf);
+  }
+}
+
+//================================================================
+void HistTDCBCSWidth::fill(const EventClass& evt, const ClustersByPlane& protonGlobalClusters) {
+  for(int iplane=1; iplane < protonGlobalClusters.size(); ++iplane) {
+    if(protonGlobalClusters[iplane].size() == 1) {
+      fill(evt, geom_->global(iplane).planeType(), protonGlobalClusters[iplane][0]);
+    }
+  }
+}
+
+//================================================================
+void HistTDCBCSWidth::fill(const EventClass& evt, WirePlane::DetType pt, const WireCluster& cluster) {
+  int sz = std::min(cluster.numCells(), maxClusterSize);
+  switch(pt) {
+  case WirePlane::PC:
+    {
+      clusterSizePC_->Fill(cluster.numCells(), cluster.plane());
+      HistTDCWidth& hbin = clusterSizeBinsPC_[sz - 1];
+      hbin.fill(cluster.hits());
+      break;
+    }
+  case WirePlane::DC:
+    {
+      clusterSizeDC_->Fill(cluster.numCells(), cluster.plane());
+      HistTDCWidth& hbin = clusterSizeBinsDC_[sz - 1];
+      hbin.fill(cluster.hits());
+      break;
+    }
+  }
+}
+
+//================================================================
---
+++ HistTDCBCSWidth.h
@@ -0,0 +1,50 @@
+// The difference from HistTDCWidth is that here we
+// histogram only selected hits from a given time window:
+// if there is more than one cluster in a plane, those
+// hits are not used.  Accepted hits are histogrammed
+// in bins of cluster size ("By Cluster Size").
+//
+// Andrei Gaponenko, 2014
+
+#ifndef HistTDCBCSWidth_h
+#define HistTDCBCSWidth_h
+
+#include <vector>
+
+#include "WireCluster.h"
+#include "HistTDCWidth.h"
+#include "DetectorGeo.h"
+
+class TH1;
+class TH2;
+
+class HistogramFactory;
+class ConfigFile;
+class DetectorGeo;
+class EventClass;
+
+//================================================================
+class HistTDCBCSWidth {
+public:
+  void init(HistogramFactory& hf,
+            const std::string& hdir,
+            const DetectorGeo& geom,
+            const ConfigFile& conf);
+
+  void fill(const EventClass& evt, const ClustersByPlane& globalClusters);
+
+private :
+  static const int maxClusterSize = 4;
+
+  const DetectorGeo *geom_;
+
+  TH2 *clusterSizePC_;
+  TH2 *clusterSizeDC_;
+
+  std::vector<HistTDCWidth> clusterSizeBinsPC_;
+  std::vector<HistTDCWidth> clusterSizeBinsDC_;
+
+  void fill(const EventClass& evt, WirePlane::DetType pt, const WireCluster& c);
+};
+
+#endif/*HistTDCBCSWidth_h*/
