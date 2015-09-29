0577cf4e210abf10a31db9109022e93b9f183b72
Andrei Gaponenko <gandr@fnal.gov>
2015-09-29 12:32:44 -0500

    Use a private copy of lib4cpp and GSL to on mu2egpvm to get a consistent build.

---
+++ setup-gpvm.sh
@@ -0,0 +1,12 @@
+# setup for  mu2egpvm machines
+export CAL_DB=/mu2e/data/users/gandr/twist/caldb_ascii
+
+setup mu2e
+###source /grid/fermiapp/products/mu2e/setupmu2e-art.sh
+
+#setup -B gsl v1_15a
+#setup -B log4cpp v1_0
+PATH=/mu2e/app/users/gandr/local/install-sl6-e7/bin:$PATH
+
+setup -B boost v1_57_0 -q +e7:+prof
+setup -B root v5_34_25 -q +e7:+prof
