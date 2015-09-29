3fe94d5ba4d8d73ac19c5641454c81f5e49f5700
Andrei Gaponenko <gandr@fnal.gov>
2015-09-29 16:55:20 -0500

    Set LD_LIBRARY_PATH for local gsl and log4cpp installs.

--- setup-gpvm.sh
+++ setup-gpvm.sh
@@ -4,9 +4,10 @@ export CAL_DB=/mu2e/data/users/gandr/twist/caldb_ascii
 setup mu2e
 ###source /grid/fermiapp/products/mu2e/setupmu2e-art.sh
 
+setup -B boost v1_57_0 -q +e7:+prof
+setup -B root v5_34_25 -q +e7:+prof
+
 #setup -B gsl v1_15a
 #setup -B log4cpp v1_0
 PATH=/mu2e/app/users/gandr/local/install-sl6-e7/bin:$PATH
-
-setup -B boost v1_57_0 -q +e7:+prof
-setup -B root v5_34_25 -q +e7:+prof
+LD_LIBRARY_PATH=/mu2e/app/users/gandr/local/install-sl6-e7/lib:$LD_LIBRARY_PATH
