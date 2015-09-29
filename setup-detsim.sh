e3d8150028e385f7804e27cdf8fd01c03126f64b
Andrei Gaponenko <gandr@fnal.gov>
2015-09-29 10:04:03 -0500

    Stricter UPS flags.

--- setup-detsim.sh
+++ setup-detsim.sh
@@ -4,7 +4,7 @@ export CAL_DB=/mu2e/data/users/gandr/twist/caldb_ascii
 ##setup mu2e
 source /grid/fermiapp/products/mu2e/setupmu2e-art.sh
 
-setup gsl v1_15a
-setup log4cpp v1_0
-setup boost v1_53_0 -q e2:prof
-setup root v5_34_05 -q e2:prof
+setup -B gsl v1_15a
+setup -B log4cpp v1_0
+setup -B boost v1_53_0 -q +e2:+prof
+setup -B root v5_34_05 -q +e2:+prof
