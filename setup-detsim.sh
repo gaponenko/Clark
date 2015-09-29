18b508087160c766b185e30b51098b52a97098f0
Andrei Gaponenko <gandr@fnal.gov>
2015-09-28 23:00:00 -0500

    Use fermiapp instead of cvmfs - some packages are missing in the latter repo.

--- setup-detsim.sh
+++ setup-detsim.sh
@@ -1,6 +1,9 @@
 # setup for detsim
 export CAL_DB=/mu2e/data/users/gandr/twist/caldb_ascii
-setup mu2e
+
+##setup mu2e
+source /grid/fermiapp/products/mu2e/setupmu2e-art.sh
+
 setup gsl v1_15a
 setup log4cpp v1_0
 setup boost v1_53_0 -q e2:prof
