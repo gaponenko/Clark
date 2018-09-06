gaponenko/Clark

commit d4f73f0fe65d66501376d8ffa36360866299fb8d

Make MuCapTrkAnalysisHF interface window-independent. Instead add a cut on track time.

MuCapture.cpp
@@ -602,10 +602,11 @@ MuCapture::EventCutNumber MuCapture::analyze(EventClass &evt, HistogramFactory &
   //----------------------------------------------------------------
   //  The pre-selection for downstream decays/captures is passed here
   // select the normalization sample
-  const int iDIONorm = dnDIONormTracks_.process(evt, muStop, decayWindow);
+  const int iDIONorm = dnDIONormTracks_.process(evt, muStop);
 
-  const int iNegTrack = dnDIOVetoTracks_.process(evt, muStop, decayWindow);
-  const int iPosTrack = dnPosTracks_.process(evt, muStop, decayWindow);
+  const int iNegTrack = dnDIOVetoTracks_.process(evt, muStop);
+  const int iPosTrack = dnPosTracks_.process(evt, muStop);
+
   const bool isPosTrackContained = dnPosTrkContainment_.contained(evt, iPosTrack, protonGlobalClusters);
   const double rangePIDVar = ((iPosTrack != -1)&& isPosTrackContained) ? hContainedProtonPID_.fill(evt, iPosTrack, protonGlobalClusters) : 0.;
 

DefaultConfig.cpp
@@ -365,7 +365,8 @@ void SetDefault(ConfigFile &Conf)
 
         //----------------
         SetKey(Conf, "MuCapture/TrkAnalysisHF/pos/cutCharge", +1);
-        SetKey(Conf, "MuCapture/TrkAnalysisHF/pos/cutTrackWinTimedt", 100.);//ns
+        SetKey(Conf, "MuCapture/TrkAnalysisHF/pos/cutTrackTimeMin", 200.);//ns
+        SetKey(Conf, "MuCapture/TrkAnalysisHF/pos/cutTrackTimeMax", 10000. - 1050.);//ns
         SetKey(Conf, "MuCapture/TrkAnalysisHF/pos/cutTrackRmax", 99999.);//cm
         SetKey(Conf, "MuCapture/TrkAnalysisHF/pos/cutCosThetaMin", 0.5);
         SetKey(Conf, "MuCapture/TrkAnalysisHF/pos/cutCosThetaMax", 0.98);
@@ -383,6 +384,8 @@ void SetDefault(ConfigFile &Conf)
         // is our use of the wire center fit.
         SetKey(Conf, "MuCapture/TrkAnalysisHF/dioNorm/cutCharge", -1);
         SetKey(Conf, "MuCapture/TrkAnalysisHF/dioNorm/cutTrackWinTimedt", 100.);//ns
+        SetKey(Conf, "MuCapture/TrkAnalysisHF/dioNorm/cutTrackTimeMin", 200.);//ns
+        SetKey(Conf, "MuCapture/TrkAnalysisHF/dioNorm/cutTrackTimeMax", 10000. - 1050.);//ns
         SetKey(Conf, "MuCapture/TrkAnalysisHF/dioNorm/cutTrackRmax",6.34);//cm = 38.0 MeV/c pt
         SetKey(Conf, "MuCapture/TrkAnalysisHF/dioNorm/cutCosThetaMin", 0.54);
         SetKey(Conf, "MuCapture/TrkAnalysisHF/dioNorm/cutCosThetaMax", 0.92);
@@ -395,7 +398,8 @@ void SetDefault(ConfigFile &Conf)
         //----------------
         // A loose set of cuts to get more DIOs for vetoing capture events
         SetKey(Conf, "MuCapture/TrkAnalysisHF/dioVeto/cutCharge", -1);
-        SetKey(Conf, "MuCapture/TrkAnalysisHF/dioVeto/cutTrackWinTimedt", 100.);//ns
+        SetKey(Conf, "MuCapture/TrkAnalysisHF/dioVeto/cutTrackTimeMin", 200.);//ns
+        SetKey(Conf, "MuCapture/TrkAnalysisHF/dioVeto/cutTrackTimeMax", 10000. - 1050.);//ns
         SetKey(Conf, "MuCapture/TrkAnalysisHF/dioVeto/cutTrackRmax",81.7);//cm, approx 49 MeV/c pt
         SetKey(Conf, "MuCapture/TrkAnalysisHF/dioVeto/cutCosThetaMin", 0.0);
         SetKey(Conf, "MuCapture/TrkAnalysisHF/dioVeto/cutCosThetaMax", 0.98);

MuCapTrkAnalysisHF.cpp
@@ -57,7 +57,8 @@ void MuCapTrkAnalysisHF::init(const std::string& hdir,
   cutStream_ = cutStream;
 
   cutCharge_ = conf.read<int>("MuCapture/TrkAnalysisHF/"+cutSet+"/cutCharge");
-  cutTrackWinTimedt_ = conf.read<int>("MuCapture/TrkAnalysisHF/"+cutSet+"/cutTrackWinTimedt");
+  cutTrackTimeMin_ = conf.read<int>("MuCapture/TrkAnalysisHF/"+cutSet+"/cutTrackTimeMin");
+  cutTrackTimeMax_ = conf.read<int>("MuCapture/TrkAnalysisHF/"+cutSet+"/cutTrackTimeMax");
   cutTrackRmax_ = conf.read<double>("MuCapture/TrkAnalysisHF/"+cutSet+"/cutTrackRmax");
   cutCosThetaMin_ = conf.read<double>("MuCapture/TrkAnalysisHF/"+cutSet+"/cutCosThetaMin");
   cutCosThetaMax_ = conf.read<double>("MuCapture/TrkAnalysisHF/"+cutSet+"/cutCosThetaMax");
@@ -80,9 +81,6 @@ void MuCapTrkAnalysisHF::init(const std::string& hdir,
   h_cuts_p->SetOption("hist text");
 
   //----------------------------------------------------------------
-  trackWinTime_ = hf.DefineTH1D(hdir, "trackWinTime", "track time - win time",
-                                201, -100.5, 100.5);
-
   hCharge_ = hf.DefineTH1D(hdir, "charge", "track charge", 3, -1.5, 1.5);
 
   hStartStop_ = hf.DefineTH2D(hdir,
@@ -171,15 +169,15 @@ void MuCapTrkAnalysisHF::init(const std::string& hdir,
                                    "max track distance from the Z axis",
                                    400, 0, 40.);
 
-  trackTime_ = hf.DefineTH1D(hdir, "trackTime", "track time",
-                             1100, -1000., 10000.);
+  trackTimeBefore_ = hf.DefineTH1D(hdir, "trackTimeBefore", "track time before cut",
+                                   1100, -1000., 10000.);
+
+  trackTimeAfter_ = hf.DefineTH1D(hdir, "trackTimeAfter", "track time after cut",
+                                  1100, -1000., 10000.);
 
   final_trackTime_ = hf.DefineTH1D(hdir, "final_trackTime",
                                    "final track time",
                                    1100, -1000., 10000.);
-
-  final_trackWinTime_ = hf.DefineTH1D(hdir, "final_trackWinTime", "final track time - win time",
-                                      201, -100.5, 100.5);
 
   hNumTracks_ = hf.DefineTH1D(hdir, "numAcceptedTracks",
                               "numAcceptedTracks",
@@ -213,12 +211,11 @@ void MuCapTrkAnalysisHF::init(const std::string& hdir,
 
 //================================================================
 int MuCapTrkAnalysisHF::process(const EventClass& evt,
-                                const ROOT::Math::XYPoint& muStopUV,
-                                const TimeWindow& protonWin)
+                                const ROOT::Math::XYPoint& muStopUV)
 {
   std::vector<unsigned> accepted;
   for(int i = 0; i < evt.ntr; ++i) {
-    if(processTrack(i, evt, muStopUV, protonWin)) {
+    if(processTrack(i, evt, muStopUV)) {
       accepted.push_back(i);
     }
   }
@@ -294,10 +291,9 @@ int MuCapTrkAnalysisHF::process(const EventClass& evt,
 //================================================================
 bool MuCapTrkAnalysisHF::processTrack(int itrack,
                                       const EventClass& evt,
-                                      const ROOT::Math::XYPoint& muStopUV,
-                                      const TimeWindow& protonWin)
+                                      const ROOT::Math::XYPoint& muStopUV)
 {
-  CutNumber c = analyzeTrack(itrack, evt, muStopUV, protonWin);
+  CutNumber c = analyzeTrack(itrack, evt, muStopUV);
   h_cuts_r->Fill(c);
   for(int cut=0; cut<=c; cut++) {
     h_cuts_p->Fill(cut);
@@ -308,8 +304,7 @@ bool MuCapTrkAnalysisHF::processTrack(int itrack,
 //================================================================
 MuCapTrkAnalysisHF::CutNumber MuCapTrkAnalysisHF::
 analyzeTrack(int i, const EventClass& evt,
-             const ROOT::Math::XYPoint& muStopUV,
-             const TimeWindow& protonWin)
+             const ROOT::Math::XYPoint& muStopUV)
 {
   //----------------------------------------------------------------
   if(evt.hefit_ierror[i] != 0) {
@@ -323,11 +318,12 @@ analyzeTrack(int i, const EventClass& evt,
   }
 
   //----------------------------------------------------------------
-  trackTime_->Fill(evt.hefit_time[i]);
-  trackWinTime_->Fill(evt.hefit_time[i] - protonWin.tstart);
-  if(std::abs(evt.hefit_time[i]  - protonWin.tstart) > cutTrackWinTimedt_) {
-    return CUT_TRKWINTIME;
+  trackTimeBefore_->Fill(evt.hefit_time[i]);
+  if( (evt.hefit_time[i] <  cutTrackTimeMin_) ||
+      (cutTrackTimeMax_ < evt.hefit_time[i]) ) {
+    return CUT_TRACKTIME;
   }
+  trackTimeAfter_->Fill(evt.hefit_time[i]);
 
   //----------------------------------------------------------------
   hStartStop_->Fill(evt.hefit_pstart[i], evt.hefit_pstop[i]);
@@ -417,7 +413,6 @@ analyzeTrack(int i, const EventClass& evt,
   final_trackRL_->Fill(evt.radius[i], evt.wavelen[i]);
   final_u0v0_->Fill(evt.hefit_u0[i], evt.hefit_v0[i]);
   final_trackTime_->Fill(evt.hefit_time[i]);
-  final_trackWinTime_->Fill(evt.hefit_time[i] - protonWin.tstart);
   final_StartStop_->Fill(evt.hefit_pstart[i], evt.hefit_pstop[i]);
 
   return CUTS_ACCEPTED;

MuCapTrkAnalysisHF.h
@@ -30,7 +30,7 @@ class MuCapTrkAnalysisHF {
   void set_cut_bin_labels(TAxis* ax) {
     ax->SetBinLabel(1+CUT_IERROR, "ierror");
     ax->SetBinLabel(1+CUT_CHARGE, "charge");
-    ax->SetBinLabel(1+CUT_TRKWINTIME, "trkwintime");
+    ax->SetBinLabel(1+CUT_TRACKTIME, "trk time");
     ax->SetBinLabel(1+CUT_STREAM, "stream");
     ax->SetBinLabel(1+CUT_RADIUS, "radius");
 
@@ -50,7 +50,7 @@ public:
   enum CutNumber {
     CUT_IERROR,
     CUT_CHARGE,
-    CUT_TRKWINTIME,
+    CUT_TRACKTIME,
     CUT_STREAM,
     CUT_RADIUS,
     CUT_COSTHETAMIN,
@@ -75,14 +75,14 @@ public:
 
   // Returns the index of an accepted track in the event, or -1
   int process(const EventClass& evt,
-              const ROOT::Math::XYPoint& muStopUV,
-              const TimeWindow& protonWin);
+              const ROOT::Math::XYPoint& muStopUV);
 
   MuCapTrkAnalysisHF()
     : doMCTruth_(false)
     , result_(nullptr)
     , cutCharge_()
-    , cutTrackWinTimedt_()
+    , cutTrackTimeMin_()
+    , cutTrackTimeMax_()
     , cutTrackRmax_()
     , cutCosThetaMin_()
     , cutCosThetaMax_()
@@ -96,8 +96,8 @@ public:
     , h_cuts_r()
     , h_cuts_p()
 
-    , trackTime_()
-    , trackWinTime_()
+    , trackTimeBefore_()
+    , trackTimeAfter_()
     , trackRL_()
     , costhVsPtot_()
     , pfine_()
@@ -115,7 +115,6 @@ public:
     , final_u0v0_()
     , final_trackROut_()
     , final_trackTime_()
-    , final_trackWinTime_()
     , final_StartStop_()
 
     , hNumTracks_()
@@ -131,7 +130,8 @@ private :
   RecoResMuCapTrk *result_;
   int cutCharge_;
   TimeWindow::StreamType cutStream_;
-  double cutTrackWinTimedt_;
+  double cutTrackTimeMin_;
+  double cutTrackTimeMax_;
   double cutTrackRmax_;
   double cutCosThetaMin_;
   double cutCosThetaMax_;
@@ -146,8 +146,8 @@ private :
   TH1 *h_cuts_p;
 
   TH1 *hCharge_;
-  TH1 *trackTime_;
-  TH1 *trackWinTime_;
+  TH1 *trackTimeBefore_;
+  TH1 *trackTimeAfter_;
 
   TH2 *trackRL_;
   TH2 *costhVsPtot_;
@@ -167,7 +167,6 @@ private :
   TH2 *final_u0v0_;
   TH1 *final_trackROut_;
   TH1 *final_trackTime_;
-  TH1 *final_trackWinTime_;
   TH2 *final_StartStop_;
 
   // Unlike the histos above, which are per track,
@@ -186,13 +185,11 @@ private :
   // true iff the track passed the cuts
   bool processTrack(int itrack,
                     const EventClass& evt,
-                    const ROOT::Math::XYPoint& muStopUV,
-                    const TimeWindow& protonWin);
+                    const ROOT::Math::XYPoint& muStopUV);
 
   CutNumber analyzeTrack(int itrack,
                          const EventClass& evt,
-                         const ROOT::Math::XYPoint& muStopUV,
-                         const TimeWindow& protonWin);
+                         const ROOT::Math::XYPoint& muStopUV);
 };
 
 #endif/*MuCapTrkAnalysisHF_h*/

commit 00485c8dcbc90486f65fde803a7e0ebd873fa22c

Use wire clusters from the track window
Fixes a breakage introduced by d4f73f0fe65d66501376d8ffa36360866299fb8d

MuCapture.cpp
@@ -203,6 +203,7 @@ bool MuCapture::Init(EventClass &E, HistogramFactory &H, ConfigFile &Conf, log4c
 
   hWindowTimeBefore_ = H.DefineTH1D(hdir, "windowTimeBeforeCut", "Decay window start time, before time cut", 1000, 0., 10000.);
   hWindowTimeAfter_ = H.DefineTH1D(hdir, "windowTimeAfterCut", "Decay window start time, after time cut", 1000, 0., 10000.);
+  hBestTrackWindDt_ = H.DefineTH1D(hdir, "bestTrackWinDt", "Best track-win time", 501, -250.5, 250.5);
 
   hNumAfterTrigWindows_ = H.DefineTH1D(hdir, "numAfterTrigTimeWindows", "numAfterTrigTimeWindows", 10, -0.5, 9.5);
   hWindow2Time_ = H.DefineTH1D(hdir, "window2Time", "Second window start time", 1000, 0., 10000.);
@@ -556,11 +557,11 @@ MuCapture::EventCutNumber MuCapture::analyze(EventClass &evt, HistogramFactory &
   //----------------------------------------------------------------
   // Veto accidental beam particles (also upstream DIOs and some protons)
 
-  const ClustersByPlane& protonGlobalClusters = afterTrigClusters[0];
+  const ClustersByPlane& afterTrig0GlobalCl = afterTrigClusters[0];
 
   int numBeamVetoHitPlanes(0);
   for(int i=1; i<=4; ++i) {
-    if(!protonGlobalClusters[i].empty()) {
+    if(!afterTrig0GlobalCl[i].empty()) {
       ++numBeamVetoHitPlanes;
     }
   }
@@ -571,7 +572,7 @@ MuCapture::EventCutNumber MuCapture::analyze(EventClass &evt, HistogramFactory &
   }
 
   for(int i=1; i<=4; ++i) {
-    if(!protonGlobalClusters[i].empty()) {
+    if(!afterTrig0GlobalCl[i].empty()) {
       hHitPCsAterBeamVeto_->Fill(i);
     }
   }
@@ -609,6 +610,13 @@ MuCapture::EventCutNumber MuCapture::analyze(EventClass &evt, HistogramFactory &
 
   const int iPosTrack = dnPosTracks_.process(evt, muStop);
 
+  const int iWinPosTrack = findTrackWindow(evt, iPosTrack, wres.windows);
+  if((iPosTrack > -1 ) && (iWinPosTrack - wres.iTrigWin < 1)) {
+    throw std::runtime_error("MuCapture: iWinPosTrack - wres.iTrigWin < 1");
+  }
+  const ClustersByPlane& protonGlobalClusters =
+    (iPosTrack > -1) ? afterTrigClusters[iWinPosTrack - wres.iTrigWin - 1] : ClustersByPlane();
+
   const bool isPosTrackContained = dnPosTrkContainment_.contained(evt, iPosTrack, protonGlobalClusters);
   const double rangePIDVar = ((iPosTrack != -1)&& isPosTrackContained) ? hContainedProtonPID_.fill(evt, iPosTrack, protonGlobalClusters) : 0.;
 
@@ -637,8 +645,13 @@ MuCapture::EventCutNumber MuCapture::analyze(EventClass &evt, HistogramFactory &
 
   hwidthMuHits_.fill(evt, muonGlobalClusters);
   if(iDIONorm != -1) {
-    hwidthDIOHits_.fill(evt, protonGlobalClusters);
-    h200nsDIO_.fill(evt, iDIONorm, protonGlobalClusters);
+    const int iWinDIONorm = findTrackWindow(evt, iDIONorm, wres.windows);
+    if(iWinDIONorm - wres.iTrigWin < 1) {
+      throw std::runtime_error("MuCapture: iWinDIONorm - wres.iTrigWin < 1");
+    }
+    const ClustersByPlane& dioGlobalClusters = afterTrigClusters[iWinDIONorm - wres.iTrigWin - 1];
+    hwidthDIOHits_.fill(evt, dioGlobalClusters);
+    h200nsDIO_.fill(evt, iDIONorm, dioGlobalClusters);
   }
 
   // Do we have any ambiguous events (both "DIO" and "proton")?
@@ -683,6 +696,28 @@ MuCapture::EventCutNumber MuCapture::analyze(EventClass &evt, HistogramFactory &
   }
 
   return CUTS_DOWNSTREAM_ACCEPTED;
+}
+
+//================================================================
+int MuCapture::findTrackWindow(const EventClass& evt,
+                               int itrack,
+                               const TimeWindowCollection& windows)
+{
+  int iwin = -1;
+
+  if(itrack > -1) {
+    double best_dt = std::numeric_limits<double>::max();
+    for(int i=0; i<windows.size(); ++i) {
+      double dt = evt.hefit_time[itrack] -  windows[i].tstart;
+      if(std::abs(dt) < best_dt) {
+        best_dt = dt;
+        iwin = i;
+      }
+    }
+    hBestTrackWindDt_->Fill(best_dt);
+  }
+
+  return iwin;
 }
 
 //================================================================

MuCapture.h
@@ -148,6 +148,7 @@ public :
 
     , hWindowTimeBefore_()
     , hWindowTimeAfter_()
+    , hBestTrackWindDt_()
     , hNumAfterTrigWindows_()
     , hWindow2Time_()
     , hWindow2dt_()
@@ -221,6 +222,7 @@ private :
 
   TH1 *hWindowTimeBefore_;
   TH1 *hWindowTimeAfter_;
+  TH1 *hBestTrackWindDt_;
 
   TH1 *hNumAfterTrigWindows_;
   TH1 *hWindow2Time_;
@@ -300,6 +302,8 @@ private :
 
   static TDCHitPreprocessing::IProcessor*
   makeTDCHitPreprocessor(const std::string& topdir, WirePlane::DetType d, HistogramFactory& hf, const DetectorGeo& geom, ConfigFile& conf);
+
+  int findTrackWindow(const EventClass& evt, int itrack, const TimeWindowCollection& windows);
 };
 
 #endif/*MuCapture_h*/